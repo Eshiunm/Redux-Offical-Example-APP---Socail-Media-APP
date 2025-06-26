bb86778bb6d8737e2494325138d94296daba9092
Michael Li <michaelli@qnap.com>
2025-06-26 17:53:45 +0800

    feat: add EditPostForm component and update routing for post editing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { PostsMainPage } from './features/posts/PostsMainPage'
 import { SinglePostPage } from './features/posts/SinglePostPage'
+import { EditPostForm } from './features/posts/EditPostForm'
 
 // 取得正確的 basename，開發環境為空，生產環境為 GitHub Pages 路徑
 const basename = process.env.NODE_ENV === 'production' ? '/Redux-Offical-Example-APP_Social-Media-APP' : ''
@@ -14,7 +15,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<PostsMainPage/>}/>
-          <Route path="/post/:postId" element={<SinglePostPage/>} />
+          <Route path="/posts/:postId" element={<SinglePostPage/>} />
+          <Route path="/editPost/:postId" element={<EditPostForm />} />
         </Routes>
       </div>
     </Router>
--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -24,13 +24,10 @@ export const AddPostForm = () => {
     const title = elements.postTitle.value
     const content = elements.postContent.value
 
-    const newPost = {
-      id : nanoid(),
-      title,
-      content
-    }
     
-    dispatch(postAdded(newPost));
+    // Now we can pass these in as separate arguments,
+    // and the ID will be generated automatically
+    dispatch(postAdded(title, content))
 
     e.currentTarget.reset()
   }
